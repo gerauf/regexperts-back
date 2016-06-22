72c560b7d876474e161d5b13c8e5b75cc63f99b1
Daniel Ortiz <danielortiz@Daniels-MacBook-Pro-5.local>
2016-06-22 18:43:01 +0100

    added hints property

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622134234) do
+ActiveRecord::Schema.define(version: 20160622174152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160622134234) do
     t.text     "mission"
     t.integer  "number"
     t.integer  "keystrokelimit"
+    t.text     "hint"
   end
 
 end
